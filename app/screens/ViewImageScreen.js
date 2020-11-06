2a37e1fc3338572ee8d82e2b31c81f91c033f039
shuvamk <kshuvam564@gmail.com>
2020-11-06 10:03:51 -0800

    Created Color Config File

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
 
+import colors from '../config/colors'
+
 const ViewImageScreen = () => {
     return (
         <View
@@ -30,19 +32,19 @@ const styles = StyleSheet.create({
     closeIcon:{
         width: 50,
         height: 50,
-        backgroundColor: "#fc5c65",
+        backgroundColor: colors.primary,
         position: "absolute",
         top: 40,
         left: 30
     },
     container:{
         flex: 1,
-        backgroundColor: '#000'
+        backgroundColor: colors.black,
     },
     deleteIcon: {
         width: 50,
         height: 50,
-        backgroundColor: "#4ecdc4",
+        backgroundColor: colors.secondary,
         position: "absolute",
         top: 40,
         right: 30
