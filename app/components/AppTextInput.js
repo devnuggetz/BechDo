92d10f508a6227c25a13360e6e195b4ab8b1a7b8
shuvamk <kshuvam564@gmail.com>
2020-11-10 13:57:06 -0800

    Fixed picker placeholder text color

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,30 +1,41 @@
-import React from 'react'
-import { Platform, StyleSheet, Text, TextInput, View } from 'react-native'
-import {MaterialCommunityIcons} from '@expo/vector-icons'
-import colors from '../config/colors'
-import defaultStyles from '../config/styles'
+import React from "react";
+import { Platform, StyleSheet, Text, TextInput, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+import defaultStyles from "../config/styles";
 
-const AppTextInput = ({icon, ...otherProps}) => {
-    return (
-        <View style={styles.container}>
-            {icon&&<MaterialCommunityIcons name={icon} size={20} color={colors.medium} style={styles.icon}/>}
-            <TextInput style={defaultStyles.text} {...otherProps}/>
-        </View>
-    )
-}
+const AppTextInput = ({ icon, ...otherProps }) => {
+  return (
+    <View style={styles.container}>
+      {icon && (
+        <MaterialCommunityIcons
+          name={icon}
+          size={20}
+          color={colors.medium}
+          style={styles.icon}
+        />
+      )}
+      <TextInput
+        placeholderTextColor={colors.medium}
+        style={defaultStyles.text}
+        {...otherProps}
+      />
+    </View>
+  );
+};
 
-export default AppTextInput
+export default AppTextInput;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: colors.light,
-        borderRadius: 25,
-        flexDirection: "row",
-        width: "100%",
-        padding: 15,
-        marginVertical: 10
-    },
-    icon: {
-        marginRight: 10,
-    }
-})
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+});
