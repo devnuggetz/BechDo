2fa94b968169dfab336afca2663635f90f7c37fc
shuvamk <kshuvam564@gmail.com>
2020-11-11 16:22:45 -0800

    Applied custom icons and color to tabs

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -5,14 +5,43 @@ import React from "react";
 import ListingsScreen from "../screens/ListingsScreen";
 import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Feed" component={FeedNavigator} />
-    <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
-    <Tab.Screen name="Account" component={AccountNavigator} />
+    <Tab.Screen
+      name="Feed"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="ListingEdit"
+      component={ListingEditScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons
+            name="plus-circle"
+            size={size}
+            color={color}
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Account"
+      component={AccountNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="account" size={size} color={color} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 export default AppNavigator;
