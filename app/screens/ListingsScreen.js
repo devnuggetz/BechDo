c0e8a1106c0abd1b7cc0eb38175b8f8fd9514de6
shuvamk <kshuvam564@gmail.com>
2020-11-13 13:31:56 -0800

    Fixed Rupee Symbol

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -33,7 +33,7 @@ function ListingsScreen({ navigation }) {
         renderItem={({ item }) => (
           <Card
             title={item.title}
-            subTitle={"$" + item.price}
+            subTitle={"₹" + item.price}
             imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
