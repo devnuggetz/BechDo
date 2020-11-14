624d06cc5ccab2eb81d855faac1a12320d7b54a2
shuvamk <kshuvam564@gmail.com>
2020-11-13 21:57:00 -0800

    Implemented the authToken

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./app/navigation/AppNavigator";
 import NavigationTheme from "./app/navigation/navigationTheme";
 import OfflineNotice from "./app/components/OfflineNotice";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 export default function App() {
   return (
     <>
       <OfflineNotice />
       <NavigationContainer theme={NavigationTheme}>
-        <AppNavigator />
+        <AuthNavigator />
       </NavigationContainer>
     </>
   );
