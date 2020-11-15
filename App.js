885f2e7d73a7d25a92635101f82345e6ff0f2d61
shuvamk <kshuvam564@gmail.com>
2020-11-14 16:00:37 -0800

    Refactored the code

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import jwtDecode from "jwt-decode";
+import { AppLoading } from "expo";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
@@ -11,14 +12,16 @@ import authStorage from "./app/auth/storage";
 
 function App() {
   const [user, setUser] = useState();
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-    setUser(jwtDecode(token));
+  const [isReady, setIsReady] = useState(false);
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
   };
-  useEffect(() => {
-    restoreToken();
-  }, []);
+  if (!isReady) {
+    return (
+      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
+    );
+  }
   return (
     <>
       <AuthContext.Provider value={{ user, setUser }}>
