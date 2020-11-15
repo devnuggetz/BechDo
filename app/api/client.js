86d509b4dc2228b75b32b9d9cd82ec0b865aa0d8
shuvamk <kshuvam564@gmail.com>
2020-11-14 16:55:02 -0800

    Implemented the Register Feature

--- app/api/client.js
+++ app/api/client.js
@@ -1,9 +1,15 @@
 import { create } from "apisauce";
+import authStorage from "../auth/storage";
 import cache from "../utility/cache";
 const apiClient = create({
   baseURL: "http://localhost:9000/api",
 });
 
+apiClient.addAsyncRequestTransform(async (request) => {
+  const authToken = await authStorage.getToken();
+  if (!authToken) return;
+  request.headers["x-auth-token"] = authToken;
+});
 const get = apiClient.get;
 apiClient.get = async (url, params, axiosConfig) => {
   const response = await get(url, params, axiosConfig);
